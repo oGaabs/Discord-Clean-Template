2de639675b0bae9c1dbb1645ece19c6cb2a38bc2
oGaabs <gabriel04.ok@gmail.com>
2023-07-30 06:31:46 -0300

    Add GoodByeMessage to guildMemberRemove event

---
+++ src/presentation/events/guildMemberRemove/GoodByeMessage.js
@@ -0,0 +1,65 @@
+const Listener = require("@domain/models/Listener")
+const { EmbedBuilder } = require("discord.js")
+
+const GuildService = require("@infrastructure/services/discord/GuildService")
+const MemberService = require("@infrastructure/services/discord/MemberService")
+const Events = require("@infrastructure/services/discord/DiscordEvents")
+
+const GUILD_ID = process.env.GUILD_ID
+const GOODBYE_CHANNEL_ID = "926556773194801153"
+const { COLORS } = require("@infra/utils/Constants")
+
+class GoodByeMessage extends Listener {
+    constructor(client) {
+        super(client, {
+            eventName: "GoodbyeMessage",
+            eventDiscord: Events.guildMemberRemove,
+            performOneTime: false,
+        })
+
+        this.memberService = null
+        this.guildService = null
+    }
+
+    async execute(member) {
+        this.memberService = new MemberService(this.client, member.guild.id, member.id)
+        this.guildService = new GuildService(this.client, member.guild.id)
+
+        if (!this.memberService.isBot(member))
+            return
+
+        if (member.guild.id == GUILD_ID)
+            this.sendMessageInGoodByeChannel(member, GOODBYE_CHANNEL_ID)
+    }
+
+    async sendMessageInGoodByeChannel(member, channelId) {
+        const welcomeChannel = await this.guildService.getChannel(channelId)
+        if (!welcomeChannel) {
+            console.log(`Channel with ID ${welcomeChannel.id} not found.`)
+            throw new Error(`Channel with ID ${welcomeChannel.id} not found.`)
+        }
+
+        const memberName = member.displayName
+        const memberImg = await this.memberService.getMemberDisplayAvatarURL(member)
+        const goodbyeEmbed = await this.buildGoodbyeEmbed(memberName, memberImg)
+
+        await welcomeChannel.send({ embeds: [goodbyeEmbed]})
+    }
+
+    async buildGoodbyeEmbed(memberName, memberImg) {
+        const embedBuilder = new EmbedBuilder()
+            .setTitle("🔴 | Parece o fim da viagem")
+            .setColor(COLORS.DEFAULT)
+            .addFields([
+                { name: `Até mais @${memberName[0].toUpperCase() + memberName.substring(1)}`, value: " **Foi uma longa jornada, mas tudo tem um fim.**" },
+            ])
+            .setThumbnail(memberImg)
+            .setDescription("Esperamos que nossos caminhos se alinhem novamente!")
+            .setFooter(await this.guildService.getFooter())
+            .setTimestamp()
+
+        return embedBuilder
+    }
+}
+
+module.exports = GoodByeMessage
