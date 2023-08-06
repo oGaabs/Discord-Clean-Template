dd6008d86955c0f1072b10cb2405d4004e6ce21b
oGaabs <gabriel04.ok@gmail.com>
2023-08-06 00:22:25 -0300

    ♻️ | refactor: Turn Logger into class and better readbility

--- src/infrastructure/utils/Logger.js
+++ src/infrastructure/utils/Logger.js
@@ -2,34 +2,39 @@ const log = require("ansi-colors")
 const moment = require("moment")
 moment.locale("pt-br")
 
-module.exports = {
-    getDate: () => {
+class Logger {
+    static getCurrentDateTime() {
         return moment().format("DD/MM, HH:mm:ss")
-    },
-    debug: function(title, content, needToInsertDate = false) {
-        title = log.green(title)
+    }
 
-        this.debugInformation(title, content, needToInsertDate)
-    },
-    error: function(title, content, needToInsertDate = false) {
-        title = log.redBright(title)
-        content = log.red(content)
+    static log(title, content, shouldInsertDateTime = false) {
+        if (shouldInsertDateTime)
+            title = `[${this.getCurrentDateTime()}]\n${title}`
 
-        this.debugInformation(title, content, needToInsertDate)
-    },
-    alert: function(title, content, needToInsertDate = false) {
-        this.error(title, content, needToInsertDate)
-    },
-    warn: function(title, content, needToInsertDate = false) {
-        title = log.bold.yellow(title)
-        content = log.yellow(content)
-
-        this.debugInformation(title, content, needToInsertDate)
-    },
-    debugInformation: function(title, content, needToInsertDate = false) {
-        if (needToInsertDate)
-            title = "[" + this.getDate() + "]\n" + title
 
         console.log(title, content)
-    },
+    }
+
+    static debug(title, content, shouldInsertDateTime = false) {
+        title = log.green(title)
+        this.log(title, content, shouldInsertDateTime)
+    }
+
+    static error(title, content, shouldInsertDateTime = false) {
+        title = log.redBright(title)
+        content = log.red(content)
+        this.log(title, content, shouldInsertDateTime)
+    }
+
+    static alert(title, content, shouldInsertDateTime = false) {
+        this.error(title, content, shouldInsertDateTime)
+    }
+
+    static warn(title, content, shouldInsertDateTime = false) {
+        title = log.bold.yellow(title)
+        content = log.yellow(content)
+        this.log(title, content, shouldInsertDateTime)
+    }
 }
+
+module.exports = Logger
