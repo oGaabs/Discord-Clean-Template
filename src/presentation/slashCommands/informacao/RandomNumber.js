75121827c79af28dc39cad5f693c7b5c0e9326ed
oGaabs <gabriel04.ok@gmail.com>
2023-08-06 00:27:28 -0300

    ♻️ | refactor: refactoring RandomNumber class

--- src/presentation/slashCommands/informacao/RandomNumber.js
+++ src/presentation/slashCommands/informacao/RandomNumber.js
@@ -1,6 +1,5 @@
 const { SlashCommandBuilder } = require("discord.js")
-const SlashCommand = require("@domain/models/SlashCommand.js")
-
+const SlashCommand = require("../../../domain/models/SlashCommand.js")
 
 class RandomNumber extends SlashCommand {
     constructor(client) {
@@ -10,12 +9,14 @@ class RandomNumber extends SlashCommand {
                 .setName("random_number")
                 .setDescription("Gera um número aleatório")
                 .addIntegerOption((option) =>
-                    option.setName("min")
+                    option
+                        .setName("min")
                         .setDescription("Digite o número mínimo")
                         .setRequired(true),
                 )
                 .addIntegerOption((option) =>
-                    option.setName("max")
+                    option
+                        .setName("max")
                         .setDescription("Digite o número máximo")
                         .setRequired(true),
                 ).toJSON(),
@@ -28,7 +29,7 @@ class RandomNumber extends SlashCommand {
     async execute(interaction, client) {
         await interaction.deferReply()
 
-        // get the args from interaction, receving channel and message to send
+        // get the min and max values
         const min = interaction.options.getInteger("min")
         const max = interaction.options.getInteger("max")
 
@@ -37,17 +38,22 @@ class RandomNumber extends SlashCommand {
     }
 
     async executeFromMessage(message, args, client) {
-        if (!args[0])
-            return client.messageService.replyMessage(message, this.generateRandomNumber().toString())
+        if (!args[0]) {
+            const randomNumber = this.generateRandomNumber()
+            client.messageService.replyMessage(message, randomNumber.toString())
+            return
+        }
 
-        const limits = args[0].split("-")
+        const limits = args[0].split(":")
         const minLimit = parseFloat(limits[0])
         const maxLimit = parseFloat(limits[1])
 
-        if (isNaN(minLimit) || isNaN(maxLimit))
-            return client.messageService.replyMessage(message, "Insira apenas números válidos!")
+        if (isNaN(minLimit) || isNaN(maxLimit)) {
+            client.messageService.replyMessage(message, "Insira apenas números válidos!")
+            return
+        }
 
-        client.messageService.replyMessage(message, this.generateRandomNumberInRange(limits[0], limits[1]).toString())
+        client.messageService.replyMessage(message, this.generateRandomNumberInRange(minLimit, maxLimit).toString())
     }
 
     generateRandomNumberInRange(min, max) { // with min and max and negative numbers
